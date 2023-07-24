ea671ed55d74686673ea781cc34d672a7dc015cd
MP <mp@swoop.pro>
2023-07-24 19:46:08 +0300

    task31 find - done;  sum of elements

---
+++ seminar/task31_Find_Sum_of_Numbers/Program.cs
@@ -0,0 +1,51 @@
+﻿// create array of 12 random elements betewwn -9 & 9
+// find sum of each positive and negative numbers
+
+// int[] array = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int[] array = new int[12];
+
+void FillArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+void CalculateSums()
+{
+    int sum_negative = 0;
+    int sum_positive = 0;
+
+    for (int i = 0; i < 12; i++)
+    {
+
+
+
+        if (array[i] < 0)
+        {
+            sum_negative = sum_negative - array[i];
+        }
+        else if (array[i] > 0)
+        {
+            sum_positive = sum_positive + array[i];
+        }
+
+    }
+    Console.WriteLine($" Positive Sum: {sum_positive}");
+    Console.WriteLine($" Negative Sum: {sum_negative*-1}");
+}
+
+FillArray();
+PrintArray();
+Console.WriteLine("");
+CalculateSums();
+Console.WriteLine("");
