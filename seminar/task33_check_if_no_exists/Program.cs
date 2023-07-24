2c652b0dbbc4d26be102105263b6965da9604b6d
MP <mp@swoop.pro>
2023-07-24 21:44:22 +0300

    task 33 dont - check if given no is inside random array

---
+++ seminar/task33_check_if_no_exists/Program.cs
@@ -0,0 +1,26 @@
+﻿// create array, check if given no is inside random array
+
+Console.WriteLine("Please input number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[4];
+string answer = "no";
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == number)
+    {
+        answer = "yes";
+    }
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}, ");
+}
+Console.WriteLine();
+Console.WriteLine(answer);
