b118c3bd8a922378a732624272fd03bd74446833
MP <mp@swoop.pro>
2023-07-24 21:07:59 +0300

    task32 done - replace positive by negative

---
+++ seminar/task32-replace_positive_by_negative/Program.cs
@@ -0,0 +1,32 @@
+﻿// create array
+// replace positive by negative and way round
+
+int[] array = new int[12];
+
+void FillArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ,");
+    }
+
+    Console.WriteLine();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i] * -1} ,");
+    }
+}
+
+FillArray();
+PrintArray();
+
+Console.WriteLine();
