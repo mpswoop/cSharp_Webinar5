c7f58f5c2c6795479c2f2b46f6c49c6dc856245a
MP <mp@swoop.pro>
2023-07-24 22:17:31 +0300

    task 35 done - count number of array elements meeting condition

---
+++ seminar/task35_random_array_find_number_of_Elements_meeding_condition/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] array = new int[123];
+int count = 0;
+
+void FillArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 1000);
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+void MatchCount()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+        {
+            count++;
+        }
+    }
+}
+
+FillArray();
+PrintArray();
+MatchCount();
+Console.WriteLine(count);
