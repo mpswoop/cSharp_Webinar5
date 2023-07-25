c1a50037fb91a660d439df9204089aefacb04561
MP <mp@swoop.pro>
2023-07-25 03:40:50 +0300

    task 37 done - multiplication of pairs

---
+++ seminar/task37_multiplication_of_pairs/Program.cs
@@ -0,0 +1,49 @@
+﻿// find product from pairs of numbers
+// regard as pair: first & last, second & prelast etc.
+
+int[] array = new int[new Random().Next(5, 11)];
+
+void FillArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+void Multiplication()
+{
+    if (array.Length % 2 == 0)
+    {
+        for (int i = 0; i < array.Length / 2; i++)
+        {
+            //   int multi = array[i] * array[array.Length-i];
+            Console.Write($"{array[i] * array[array.Length - 1 - i]}, ");
+        }
+    }
+    else 
+    {
+        for (int i = 0; i < array.Length / 2; i++)
+        {
+            //   int multi = array[i] * array[array.Length-i];
+            Console.Write($"{array[i] * array[array.Length - 1 - i]}, ");
+        }
+        Console.Write(array[array.Length/2]);
+    }
+
+}
+
+FillArray();
+Console.WriteLine("");
+PrintArray();
+Console.WriteLine("");
+Multiplication();
+Console.WriteLine("");
