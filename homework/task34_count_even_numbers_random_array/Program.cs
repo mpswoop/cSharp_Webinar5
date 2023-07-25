3d04fc9b11308e3b2d490a39fb057ce0b6af35a6
MP <mp@swoop.pro>
2023-07-25 04:02:24 +0300

    task34 done - count even elements of array

---
+++ homework/task34_count_even_numbers_random_array/Program.cs
@@ -0,0 +1,42 @@
+﻿// array of random positive 3-digit numbers
+// amount of even numbers in array
+
+int[] array = new int[new Random().Next(1, 100)];
+int count = 0;
+
+void FillArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+void CountEven()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine(count);
+}
+
+Console.WriteLine("");
+FillArray();
+Console.WriteLine("");
+PrintArray();
+Console.WriteLine("");
+CountEven();
+Console.WriteLine("");
