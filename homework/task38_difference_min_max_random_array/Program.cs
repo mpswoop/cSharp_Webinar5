bc0920958a2bb5a9e5bdf1597f98524311afb9e6
MP <mp@swoop.pro>
2023-07-25 04:40:50 +0300

    task38 done - difference between max and min of an array

---
+++ homework/task38_difference_min_max_random_array/Program.cs
@@ -0,0 +1,49 @@
+﻿// create random array
+// find difference between min and max
+
+int[] array = new int[new Random().Next(5, 8)];
+int max = array[0];
+int min = 1000;
+
+void FillArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+void MinMax()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    Console.WriteLine();
+    // Console.WriteLine(min);
+    // Console.WriteLine(max);
+    Console.WriteLine(max - min);
+}
+
+Console.WriteLine("");
+FillArray();
+Console.WriteLine("");
+PrintArray();
+Console.WriteLine("");
+MinMax();
+Console.WriteLine("");
