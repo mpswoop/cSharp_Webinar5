dc89cc36af0bd6261dc33c59c5855697fb0c96e3
MP <mp@swoop.pro>
2023-07-25 04:15:40 +0300

    task36 done -find sum of uneven elements of a random array

---
+++ homework/task36_sum_elements_uneven_positions/Program.cs
@@ -0,0 +1,42 @@
+﻿// create random array
+// find sum of uneven elements
+
+int[] array = new int[new Random().Next(10, 100)];
+int sum = array[0];
+
+void FillArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+void SumUneven()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((i+1) % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine(sum);
+}
+
+Console.WriteLine("");
+FillArray();
+Console.WriteLine("");
+PrintArray();
+Console.WriteLine("");
+SumUneven();
+Console.WriteLine("");
